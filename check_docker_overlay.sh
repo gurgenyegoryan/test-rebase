ce4261588595b8a1c5159e03881595c92399bf0e
Gurgen <gurgen.yegoryan@gmail.com>
2023-05-31 11:09:28 +0400

    Test

--- check_docker_overlay.sh
+++ check_docker_overlay.sh
@@ -3,32 +3,32 @@
 while true
 do
     LIMIT="10"
-
+    
     # Overlay2 folder
     path=/var/lib/docker/overlay2/
-
+    
     # Check the current size
     CHECK="`/usr/bin/du -sh $path | /usr/bin/cut -f1`"
-
+    
     # Check if folder size 0 or not
     if [ $CHECK -eq 0 ];then
         echo "Folder size is 0,nothing to do"
         /usr/bin/sleep 3600
         continue
     fi
-
+    
     echo "Current Foldersize: $CHECK GB"
-
+    
     # Get last character and remove it
     last_char="`echo "${CHECK:0-1}"`"
     CHECK="`echo ${CHECK:0:-1}`"
-
+    
     # Check size and if neccesary delete/prune docker system
     if [ $last_char == "M" ] || [ $last_char == "K" ];then
         echo "Its megabyte"
         /usr/bin/sleep 3600
         continue
-    elif [ $CHECK -ge $LIMIT ];then
+        elif [ $CHECK -ge $LIMIT ];then
         echo "Start prune docker"
         docker system prune -a --volumes
         /usr/bin/sleep 3600
@@ -37,3 +37,6 @@ do
         break
     fi
 done
+
+
+# add test comment
